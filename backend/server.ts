5a8f1e7ad98ed95446a374e8ae52dfa3cda0d251
Ajay Kumar <ajaykrsingh8709@gmail.com>
2026-03-25 22:59:37 +0530

    fixed cors problem

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://alphafittrack.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
